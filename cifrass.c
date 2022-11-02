7b5d8ef8c18301831b8f01d46edb3c7b08e41943
Y0SDan <73656015+Y0SDan@users.noreply.github.com>
2022-11-01 18:03:17 -0600

    last versio for now

---
+++ cifrass.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    
+    printf("Introduce un numero: \n");
+    scanf("%i",&n);
+
+    if(n < 10){
+        switch(n){
+            case 0: printf("cero\n"); break;
+            case 1: printf("uno\n"); break;
+            case 2: printf("dos\n"); break;
+            case 3: printf("tres\n"); break;
+            case 4: printf("cuatro\n"); break;
+            case 5: printf("cinco\n"); break;
+            case 6: printf("seis\n"); break;
+            case 7: printf("siete\n"); break;
+            case 8: printf("ocho\n"); break;
+            case 9: printf("nueve\n"); break;
+        }
+    }
+
+    return 0;
+}
--- fechas.c
+++ fechas.c
@@ -3,4 +3,23 @@
 int main{
     int n;
     
+    printf("Introduce un numero: \n");
+    scanf("%i",&n);
+
+    if(n < 10){
+        switch(n){
+            case 0: printf("cero\n");
+            case 1: printf("uno\n");
+            case 2: printf("dos\n");
+            case 3: printf("tres\n");
+            case 4: printf("cuatro\n");
+            case 5: printf("cinco\n");
+            case 6: printf("seis\n");
+            case 7: printf("siete\n");
+            case 8: printf("ocho\n");
+            case 9: printf("nueve\n");
+        }
+    }
+
+    return 0;
 }
